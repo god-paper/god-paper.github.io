bbdf3ad2e911506ed68a508e2d60b8f80df47e8f
god-paper <133510220+god-paper@users.noreply.github.com>
2023-05-13 23:49:11 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const text = "got-paper.github.io";
+let index = 0;
+
+function typeText() {
+  const element = document.getElementById("text");
+  element.innerText = text.slice(0, index);
+  index++;
+  if (index > text.length) {
+    clearInterval(intervalId);
+  }
+}
+
+const intervalId = setInterval(typeText, 100);
+
